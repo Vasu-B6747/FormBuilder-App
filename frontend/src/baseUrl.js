ad58ba5ce2c98b395ce46beb463f98b503ad2b18
Vasu-B6747 <boyavasu7811@gmail.com>
2025-08-02 12:59:05 +0530

    update baseUrl

--- frontend/src/baseUrl.js
+++ frontend/src/baseUrl.js
@@ -1,4 +1,4 @@
 import axios from 'axios'
 export default axios.create({
-    baseURL:'http://localhost:3010'
+    baseURL:"https://formbuilder-app-1.onrender.com"             //'http://localhost:3010'
 })
